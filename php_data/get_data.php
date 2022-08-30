cc4f33361516301d5e61a8cc03079fe287626fb7
kislayraj <kislayrajai06@gmail.com>
2022-08-31 00:51:40 +0530

    Database connection file config.php and login detail file get_data.php was added

---
+++ php_data/get_data.php
@@ -0,0 +1,33 @@
+<?php
+
+include "config.php";
+try{
+
+ $input = file_get_contents("php://input");
+ $decode = json_decode($input,true);
+
+ $name = mysqli_real_escape_string($conn,$decode['name']);
+
+ $sql = "SELECT u_name, u_pass from user_detail where u_name ='{$name}'" or die("Selection failed");
+
+ $result = mysqli_query($conn,$sql) or die("Query Failed");
+
+ $output = [] ; 
+ if(mysqli_num_rows($result) > 0){
+    $row = mysqli_fetch_assoc($result);
+
+   $output['name'] = $row["u_name"]; 
+   $output['password'] = $row["u_pass"]; 
+ }
+
+ echo  json_encode($output);
+
+}catch(Exception $e){
+    echo json_encode($e->getMessage());
+}
+
+
+
+
+
+?>
